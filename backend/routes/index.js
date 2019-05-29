e0319c895c44fb1b4502f96aa2805da15533ae14
jakyoung <wkrud203@gmail.com>
2019-05-29 20:50:58 +0900

    commit

---
+++ backend/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'))
+});
+
+router.get('/test', function(req, res, next) {
+  res.send('test with a test');
+});
+
+module.exports = router;
